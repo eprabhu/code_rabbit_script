a9858d4d51121af5042db7fa3d1f55b8ccf7014e
prabhu e <121430466+eprabhu@users.noreply.github.com>
2026-07-13 12:50:29 +0530

    test

--- test/sql/bad_example.sql
+++ test/sql/bad_example.sql
@@ -1,6 +1,6 @@
 -- Intentionally broken SQL file for testing the validator
 
-SELET id, name FORM users WHERE status = 'active;
+SELET id, name FORM users WHERE status = 'active';
 
 INSTERT INTO orders (id, total) VALUES (1, 100);
 
