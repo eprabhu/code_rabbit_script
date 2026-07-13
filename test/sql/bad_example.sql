9ae6ae9584f70c9bc9f354b7996b3ceba2917dca
prabhu e <121430466+eprabhu@users.noreply.github.com>
2026-07-13 12:48:20 +0530

    Update bad_example.sql
    test

--- test/sql/bad_example.sql
+++ test/sql/bad_example.sql
@@ -2,10 +2,10 @@
 
 SELET id, name FORM users WHERE status = 'active;
 
-INSTERT INTO orders (id, total) VALUES (1, 100)
+INSTERT INTO orders (id, total) VALUES (1, 100);
 
 DROP TABLE old_logs;
 
 ALTER TABLE users ADD COLUMN last_login DATETIME;
 
-SET @QUERY_STATEMENT = 'SELECT 1'
+SET @QUERY_STATEMENT = 'SELECT 1';
